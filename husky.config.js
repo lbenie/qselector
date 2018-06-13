e8a288997ab9741153782d72187704d1c15f83ec
Lucien Bénié <lucien.benie@gmail.com>
2018-06-13 19:57:05 -0400

    fix(husky): fixes deploy command

--- husky.config.js
+++ husky.config.js
@@ -3,6 +3,6 @@ module.exports = {
     'commit-msg': 'commitlint -e $GIT_PARAMS',
     'pre-commit': 'lint-staged',
     'post-merge': 'sh post-merge.sh',
+    'post-commit': 'git update-index --again',
   },
 };
-
