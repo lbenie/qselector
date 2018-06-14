ea44af45204d921c72996b66993da7d28a1fac01
Lucien Bénié <lucien.benie@gmail.com>
2018-06-13 20:20:44 -0400

    ci:skip

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -33,6 +33,7 @@ gulp.task('changelog', () => gulp
 gulp.task('github-release', done => github({
   type: 'oauth',
   token: process.env.CONVENTIONAL_GITHUB_RELEASER_TOKEN,
+  url: 'https://api.github.com/',
 }, { preset: process.env.PRESET || 'eslint' }, done));
 
 gulp.task('bump-version', () => gulp
